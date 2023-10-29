0cb7ef5d075e81373eb73b91762a6907f56ca389
Alan-Iriarte <141943001+Alan-Iriarte@users.noreply.github.com>
2023-10-29 16:54:54 -0300

    Add files via upload

---
+++ Js/carritoBeblible.js
@@ -0,0 +1,141 @@
+let carrito = [];
+
+async function cargarProductos() {
+  try {
+    const response = await fetch("/Json/productosBebible.json");
+    const data = await response.json();
+    const productosContainer = document.getElementById("productosContainer");
+
+    data.forEach((producto) => {
+      const productoDiv = document.createElement("div");
+      productoDiv.className = "col-lg-4";
+      productoDiv.innerHTML = `
+                        <img src="${producto.imagen}" alt="${
+        producto.nombre
+      }" width="200">
+               <h3>${producto.nombre}</h3>
+               <p>Precio: $${producto.precio.toFixed(2)}</p>
+              <button class="btn btn-primary  " onclick="agregarAlCarrito(${
+                producto.id
+              }, '${producto.nombre}', ${
+        producto.precio
+      })">Agregar al Carrito</button>
+                    `;
+
+      productosContainer.appendChild(productoDiv);
+    });
+  } catch (error) {
+    console.error("Error al cargar los productos:", error);
+  }
+}
+
+cargarProductos();
+
+function mostrarNotificacion(producto, tipo) {
+  Toastify({
+      text: `Producto agregado al carrito: ${producto}`,
+      duration: 2000,
+      close: true,
+      gravity: "top",
+      position: "right",
+      backgroundColor: tipo === "success" ? "green" : "red",
+  }).showToast();
+}
+
+function agregarAlCarrito(id, nombre, precio,) {
+  if (carrito.some((item) => item.id === id)) {
+    carrito.forEach((item) => {
+      if (item.id === id) {
+        item.cantidad += 1;
+        item.total += precio;
+      }
+    });
+  } else {
+    carrito.push({ id, nombre, precio, cantidad: 1, total: precio });
+  }
+
+  guardarCarritoEnLocalStorage();
+  actualizarCarrito();
+  mostrarNotificacion(nombre, "success");
+
+}
+
+
+
+
+function vaciarCarrito() {
+  Swal.fire({
+    title: "¿Estás seguro de que deseas vaciar el carrito?",
+    text: "Esta acción no se puede deshacer.",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Sí, vaciar carrito",
+    cancelButtonText: "Cancelar",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      carrito = [];
+      guardarCarritoEnLocalStorage();
+      actualizarCarrito();
+      Swal.fire("Carrito vaciado", "", "success");
+    } else {
+      Swal.fire("Operación cancelada", "", "info");
+    }
+  });
+}
+
+function eliminarDelCarrito(id) {
+  const index = carrito.findIndex((item) => item.id === id);
+  if (index !== -1) {
+    const item = carrito[index];
+    if (item.cantidad > 1) {
+      item.cantidad -= 1;
+      item.total -= item.precio;
+    } else {
+      carrito.splice(index, 1);
+    }
+    guardarCarritoEnLocalStorage();
+  }
+  actualizarCarrito();
+}
+
+function actualizarCarrito() {
+  const carritoElement = document.getElementById("carrito");
+  carritoElement.innerHTML = "";
+  let totalCarrito = 0;
+
+  carrito.forEach((item) => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+                    <td>${item.nombre}</td>
+                    <td>${item.cantidad}</td>
+                    <td>$${item.precio.toFixed(2)}</td>
+                    <td>$${item.total.toFixed(2)}</td>
+                    <td><button class="btn btn-danger btn-sm" onclick="eliminarDelCarrito(${
+                      item.id
+                    })">Eliminar</button></td>
+                    
+                    
+                `;
+    carritoElement.appendChild(tr);
+    totalCarrito += item.total;
+  });
+
+
+  const totalElement = document.getElementById("totalCarrito");
+  totalElement.textContent = `$${totalCarrito.toFixed(2)}`;
+}
+
+function guardarCarritoEnLocalStorage() {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+function cargarCarritoDesdeLocalStorage() {
+  const carritoEnLocalStorage = localStorage.getItem("carrito");
+  if (carritoEnLocalStorage) {
+    carrito = JSON.parse(carritoEnLocalStorage);
+    actualizarCarrito();
+  }
+}
+
+cargarCarritoDesdeLocalStorage();
+
---
+++ Js/checkout.js
@@ -0,0 +1,82 @@
+function cargarProductosDesdeLocalStorage() {
+  const productTable = document.getElementById("productTable");
+
+  const productos = JSON.parse(localStorage.getItem("carrito")) || [];
+
+  productTable.innerHTML = "";
+
+  let totalCarrito = 0;
+
+  productos.forEach((producto) => {
+    const row = document.createElement("tr");
+    const precioTotal = (
+      parseFloat(producto.precio) * parseFloat(producto.cantidad)
+    ).toFixed(2);
+    totalCarrito += parseFloat(precioTotal);
+
+    row.innerHTML = `
+            <td>${producto.nombre}</td>
+            <td>${producto.precio}</td>
+            <td>${producto.cantidad}</td>
+            <td>${precioTotal}</td>
+        `;
+    productTable.appendChild(row);
+  });
+
+  document.getElementById("totalCarrito").textContent = totalCarrito.toFixed(2);
+}
+
+cargarProductosDesdeLocalStorage();
+
+function calcularMontoTotal(carrito) {
+  var total = 0;
+  carrito.forEach(function (producto) {
+    total += producto.precio * producto.cantidad;
+  });
+  return total;
+}
+
+function mostrarMontoTotal() {
+  var carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+  if (carrito.length > 0) {
+    var montoTotal = calcularMontoTotal(carrito);
+    Swal.fire({
+      icon: "success",
+      title: "Pago exitoso",
+      text: "Monto total: $" + montoTotal.toFixed(2),
+    }).then(function (result) {
+      localStorage.removeItem("carrito");
+      window.history.back();
+    });
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Carrito vacío",
+      text: "Agrega productos al carrito antes de pagar",
+    });
+  }
+}
+
+document.getElementById("pay-button").addEventListener("click", function () {
+  var nombre = document.getElementById("nombre").value;
+  var tarjeta = document.getElementById("tarjeta").value;
+  var expiracion = document.getElementById("expiracion").value;
+  var cvv = document.getElementById("cvv").value;
+
+  // Verifica si los campos están llenos
+  if (
+    nombre.trim() === "" ||
+    tarjeta.trim() === "" ||
+    expiracion.trim() === "" ||
+    cvv.trim() === ""
+  ) {
+    Swal.fire({
+      icon: "error",
+      title: "Campos incompletos",
+      text: "Por favor, completa todos los campos antes de pagar",
+    });
+  } else {
+    mostrarMontoTotal();
+  }
+});
